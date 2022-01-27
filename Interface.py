dd83aa26ac5b57d9e8916aab275469fdfc96beb1
Guilherme Feitosa <guifcm12@gmail.com>
2022-01-27 15:46:52 -0300

    Interface upgrade

---
+++ Interface.py
@@ -0,0 +1,71 @@
+import os
+import time
+
+class Interface:
+    def __init__(self, menu = { 'Não foram passadas funcionalidades.': None }, nomeMenu = 'Menu'):
+        self.__menu = menu
+        self.nomeMenu = nomeMenu
+
+    def start(self):
+        try:
+            self.__MainFlag = True
+            while self.__MainFlag:
+                self.execute()  
+                self.__limpar()
+      
+        except KeyboardInterrupt: print('\n\nPrograma interrompido pelo usuário!')
+
+    def execute(self):
+        self.show()
+        opcao = self.opcao()
+
+        if opcao == 0: self.sair()
+        else:
+            funcao = self.__funcao(opcao - 1)
+            retorno = funcao()
+            if retorno: print(f'\n{retorno}')
+
+    def addFuncionalidade(self, funcionalidade = {}):
+        for nome, func in funcionalidade.items(): self.__menu[nome] = func
+
+    def removeFuncionalidade(self, nome): del self.__menu[nome]
+
+    def show(self):
+        self.exibirLogo()
+        for idx, menu in enumerate(self.__menu.keys()): print(f'  [{idx + 1}] {menu}')
+        print('  [0] Sair')
+    
+    def opcao(self): 
+        while True:
+            try: 
+                opcao = int(input('\nOpção: '))
+
+                if opcao > len(self.__menu) or opcao < 0: print('Opção inválida. Tente novamente!')
+                else: return opcao
+            except ValueError: print('Opção inválida. Tente novamente!')
+
+    def exibirLogo(self): self.nomeMenu != '' and print(f'\n <-- {self.nomeMenu} -->\n')
+
+    def __limpar(self):
+        input('\nENTER Para Prosseguir...')
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def __funcao(self, idx):
+        for i, funcao in enumerate(self.__menu.values()): 
+            if idx == i: return funcao
+
+    def sair(self): 
+        print('Finalizando processos e salvando dados...')
+        time.sleep(1.5)
+        print('Obrigado por utiliza nosso software!')
+        self.__breakMainLoop()
+    
+    def __breakMainLoop(self): self.__MainFlag = False
+    
+
+# interface = Interface()
+# interface.setMenu({
+#     'Somar': lambda : 2 + 1,
+#     'Subtrair': lambda : 2 - 1
+# }, 'Interface')
+# interface.start()
---
+++ interfaceFunctions.py
@@ -0,0 +1,96 @@
+from IceCakeOnline import IceCakeOnline
+from Interface import Interface
+
+cake = None
+
+def dadosEmpresa(): 
+    def cadastrarAtualizarDados():
+        global cake
+        name = input('Nome do Restaurante: ')
+        typ = input('Tipo de Cozinha....: ')
+        cake = IceCakeOnline(name, typ)
+        print('Sua empresa foi cadastrada com sucesso!')
+    
+    def consultarDados():
+        try: cake.describe_restaurant()
+        except AttributeError: print('Empresa Não Cadastrada')
+
+    Interface({
+        'Cadastrar/Atualizar Dados': cadastrarAtualizarDados,
+        'Consultar Dados': consultarDados
+    }).execute()
+
+def inserirAtualizarProdutosEstoque(): 
+        print('\n--- Adição de Produtos no Estoque ---')
+        try:
+            name = input('Sabor ....: ').capitalize()
+            quantity = int(input('Quantidade: '))
+            price = float(input('Preço RS..: '))
+
+            cake.addFlavor(name, { 'estoque': quantity, 'preco': price })
+        except ValueError: print('Algum valor inválido. Tente novamente.')
+        except AttributeError: print('Empresa Não Cadastrada')
+
+def consultarEstoque(): 
+    try: 
+        print(' \n--- Produtos no Estoque ---')
+        print('| Sabor | Quantidade | Preço RS |')
+        cake.show_flavors()
+    except AttributeError: print('Empresa Não Cadastrada')  
+
+def registrarPedido(): 
+    try:
+        print('\n--- Dados para Entrega ---')
+        while True:
+            cep = input('CEP: ')
+            endereco = cake.verificarCep(cep)
+            if endereco:
+                complemento = input('Complemento ....: ')
+                break
+            else: print('CEP Inválido. Tente novamente')
+
+        listaPedido = []
+
+        print('\n--- Registrar Pedido ---')
+        while True:
+            flavor = input('Sabor do Bolo ..: ')
+            quantity = input('Quantidade .....: ')
+
+            if cake.verificarPedido(flavor, quantity):
+                listaPedido.append(cake.contruirPedido(flavor, quantity))
+                continuar = input('Outro Sabor? [S / N]: ')
+                
+                if continuar.lower() != 's':
+                    endereco = cake.addComplementoEndereco(complemento, endereco)
+                    numeroPedido = cake.fazerPedido(listaPedido, endereco)
+                    print('Numero do seu pedido:', numeroPedido)
+                    break
+            else: 
+                print('Sabor inválido ou estoque insuficiente.')
+                if input('Tentar Novamente? [S / N]: ').lower() == 'n': break
+
+    except AttributeError: print('Empresa Não Cadastrada')
+
+def alterarPedido(): 
+    try: 
+        numeroPedido = input('\nNúmero do Pedido: ')
+        print(f'Pedido {numeroPedido} alterado!')
+    except AttributeError: print('Empresa Não Cadastrada')
+
+def consultarPedido(): 
+    try: cake.showPedido()
+    except AttributeError: print('Empresa Não Cadastrada')
+
+def entregarPedido(): 
+    try:
+        if len(cake.pedidos) > 0:
+            resp = input('Pedido foi entregue? [S/N]: ')
+            entregue = True if resp.lower() == 's' else False
+            status = input('Status: ')
+            cake.atenderPedido(entregue, status)
+        else: print('\nNão há pedidos para serem entregues...')
+    except AttributeError: print('Empresa Não Cadastrada')
+
+def consultarPedidosEntregues():
+    try: cake.pedidosEntregues()
+    except AttributeError: print('Empresa Não Cadastrada')
